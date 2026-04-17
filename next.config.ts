cfd6f5ae03d0d98b74fa9e9a5ebdf7c5c4dcbbd2
Mahmoud Soufi <soufi.m.ahmed@gmail.com>
2026-04-18 02:29:43 +0300

    fix: remote image patterns

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ import { withPayload } from '@payloadcms/next/withPayload'
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.vercel-storage.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 export default withPayload(nextConfig)
